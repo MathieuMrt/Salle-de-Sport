fa8152ae233f516bd92cfe005bf00cdc5dec7445
J-E <beillevertj.contact@gmail.com>
2023-03-17 14:50:06 +0100

    Intégration page accueil + modification footer balise main v2

---
+++ Main/index.js
@@ -0,0 +1,23 @@
+window.addEventListener('scroll', function(e) {
+
+    // Apparition conditionnelle du footer en fonction de la hauteur de la page
+
+    
+    let footer = document.querySelector('footer')
+    
+    
+    
+    let scrollPosition = window.pageYOffset;
+    const totalHeight = document.body.scrollHeight;
+    const windowHeight = window.innerHeight;
+    
+    
+    if (scrollPosition >= (totalHeight - windowHeight)) {
+        console.log("hey");
+        footer.style.transform = "translateY(+100%)";
+
+    } else {
+        footer.style.transform = "translateY(+100%)";
+
+    }
+})
