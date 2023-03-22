e773267c307c3390c7c069c2d5131b15571171ad
J-E <beillevertj.contact@gmail.com>
2023-03-22 17:13:38 +0100

    mise en place du slider js

---
+++ pages/Accueil/accueil.js
@@ -0,0 +1,26 @@
+
+
+const sliderContent = [
+    {
+        img: "/assets/Img/slider-img-activite.png",
+        title: "Nos activités",
+    },
+    {
+        img: "/assets/Img/slider-img-coachs.png",
+        title: "Nos coachs",
+    },
+    {
+        img: "/assets/Img/slider-img-tarifs.png",
+        title: "Nos tarifs",
+    }
+]
+
+const imgSlider = document.querySelector('.img-card')
+const titleSlider = document.querySelector('#h3')
+const nextPageSlider = document.querySelector('.button-next-page')
+
+
+
+
+
+
