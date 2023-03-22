96d1a6b66b4a29222ca94b98c21db39a1c29ae65
CLENALIO <caroline.le.nalio@gmail.com>
2023-03-22 14:30:27 +0100

    pop up stylise

--- pages/Contact/contact.js
+++ pages/Contact/contact.js
@@ -2,23 +2,31 @@ const form = document.querySelector(".formulaire");
 const prenom = document.getElementById('prenom');
 const email = document.getElementById('email');
 const message = document.getElementById('message');
+const alerte = document.querySelector(".alert");
+const alertMessage = document.querySelector(".alertMessage");
+const container = document.querySelector("container-1000");
 
+alerte.style.visibility= "hidden";
 
 form.onsubmit = function (event) {
 if (prenom.value === '' || prenom.value == null) {
     event.preventDefault();
-    alert("🦖 Merci d'indiquer votre prénom 🦖")
+    alerte.style.visibility= "visible";
+    alertMessage.innerHTML = "🦖 Merci d'indiquer votre prénom 🦖";
 }
 else if (email.value === '' || email.value == null) {
-    event.preventDefault();
-    alert("🦖 Merci d'indiquer votre email 🦖")
+    event.preventDefault(); 
+    alerte.style.visibility= "visible";
+    alertMessage.innerHTML = "🦖 Merci d'indiquer votre email 🦖";
 }
 else if (message.value === '' || message.value == null) {
-    event.preventDefault();
-    alert("🦖 Merci d'indiquer votre message 🦖")
+    event.preventDefault(); 
+    alerte.style.visibility= "visible";
+    alertMessage.innerHTML = "🦖 Merci d'indiquer votre message 🦖";
 }
 else {
-    event.preventDefault();
-    alert("🦖 Merci " + prenom.value + " pour votre message. Nous vous apporterons un retour dans les meilleurs délais. 🦖");
+    event.preventDefault(); 
+    alerte.style.visibility= "visible";
+    alertMessage.innerHTML = "🦖 Merci " + prenom.value + " pour votre message. Nous vous apporterons un retour dans les meilleurs délais. 🦖";
 }
 };
