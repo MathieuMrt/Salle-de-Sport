87f37462a64ec4ac65503eb42aa545d866c3debc
CLENALIO <caroline.le.nalio@gmail.com>
2023-03-22 09:40:00 +0100

    ajout animation et alerte javascript

--- pages/Contact/contact.js
+++ pages/Contact/contact.js
@@ -1,6 +1,24 @@
 const form = document.querySelector(".formulaire");
+const prenom = document.getElementById('prenom');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
 
 form.onsubmit = function (event) {
+if (prenom.value === '' || prenom.value == null) {
     event.preventDefault();
-    alert("Merci pour votre message. Nous vous apporterons un retour dans les meilleurs délais.");
+    alert("🦖 Merci d'indiquer votre prénom 🦖")
+}
+else if (email.value === '' || email.value == null) {
+    event.preventDefault();
+    alert("🦖 Merci d'indiquer votre email 🦖")
+}
+else if (message.value === '' || message.value == null) {
+    event.preventDefault();
+    alert("🦖 Merci d'indiquer votre message 🦖")
+}
+else {
+    event.preventDefault();
+    alert("🦖 Merci " + prenom.value + " pour votre message. Nous vous apporterons un retour dans les meilleurs délais. 🦖");
+}
 };
